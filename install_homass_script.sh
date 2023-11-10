68e556916a525439970d4038531f24964302eba3
Bagunda <shururik@gmail.com>
2023-11-10 19:02:33 +0200

    Update install_homass_script.sh
    os-agent_1.6.0_linux_x86_64.deb

--- install_homass_script.sh
+++ install_homass_script.sh
@@ -7,8 +7,8 @@ systemctl enable NetworkManager
 curl -fsSL get.docker.com | sh
 gpasswd -a $USER docker
 newgrp docker
-wget https://github.com/home-assistant/os-agent/releases/download/1.5.1/os-agent_1.5.1_linux_x86_64.deb
-dpkg -i os-agent_1.5.1_linux_x86_64.deb
+wget https://github.com/home-assistant/os-agent/releases/download/1.6.0/os-agent_1.6.0_linux_x86_64.deb
+dpkg -i os-agent_1.6.0_linux_x86_64.deb
 wget https://github.com/home-assistant/supervised-installer/releases/download/1.4.3/homeassistant-supervised.deb
 dpkg -i homeassistant-supervised.deb
 wget -O - https://get.hacs.xyz | bash -
